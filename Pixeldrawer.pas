f07019d8c7191ffd99879f260c7ce669167764eb
Err01 <132213761+Err01-Hub20@users.noreply.github.com>
2023-05-06 12:40:28 +0200

    Create Pixeldrawer.pas

---
+++ Pixeldrawer.pas
@@ -0,0 +1,23 @@
+program draw_pixel; 
+Uses Crt, Graph; 
+
+Var 
+graphicsDriver, graphicsMode, 
+color, maxColor, startX, startY : integer; 
+
+Begin
+graphicsDriver := Detect;
+InitGraph(graphicsDriver, graphicsMode, ''); 
+
+startX := getMaxX; 
+startY := getMaxY; 
+
+randomize; 
+While (not keypressed) do 
+Begin 
+color := random(maxColor) + 1; 
+putPixel(random(startX), random(startY), color); 
+end; 
+  Closegraph;
+  End. 
+  
